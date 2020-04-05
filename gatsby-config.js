94957fba557b3a940a28f830d24449d41aaf97e9
DutraNeto <dudutraneto@gmail.com>
2020-04-05 01:15:29 -0400

    Cleanup starter kit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `The Dude Developer`,
+    description: `A blog about web development.`,
+    author: `JDutra`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
